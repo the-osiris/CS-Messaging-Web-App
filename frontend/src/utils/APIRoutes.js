be74b53db22fca1562e48e7106b7053d66cd2c46
the-osiris <dev.kunal.singh@gmail.com>
2023-10-24 13:18:22 +0530

    Adding new API key for GPT

--- frontend/src/utils/APIRoutes.js
+++ frontend/src/utils/APIRoutes.js
@@ -1,4 +1,5 @@
-export const host = "http://localhost:5000";
+export const host =
+  "https://ec2-15-207-21-133.ap-south-1.compute.amazonaws.com/";
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 export const allUsersRoute = `${host}/api/auth/users`;
